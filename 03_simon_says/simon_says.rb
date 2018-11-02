51ba5462a61fe0fdea5a50ab8f3435a6633e6d5f
Colton Shaheen <coltonjshaheen@gmail.com>
2018-11-02 15:39:41 -0400

    Finished through pig_latin.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -11,3 +11,25 @@ def repeat(phrase, number = 2)
   phrase = phrase * number
   phrase.strip!
 end
+
+def start_of_word(phrase, number_of_letters = 1)
+  word_beginning = phrase[0, number_of_letters]
+end
+
+def first_word(phrase)
+  phrase.split()[0]
+end
+
+def titleize(phrase)
+  phrase_array = phrase.split()
+
+  phrase_array[0].capitalize!
+  
+  phrase_array.each do |word|
+    if word != 'the' && word != 'and' && word != 'over'
+      word.capitalize!
+    end
+  end
+
+  phrase_titleized = phrase_array.join(' ')
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
-#write your code here
+def translate(phrase)
+  phrase_array = phrase.split(' ')
+
+  phrase_array_translated = phrase_array.map do |word|
+    vowel_index = word.index(/[aeiou]/)
+    word_rearranged = word
+  
+    if vowel_index != 0
+      word_array = word.partition(/[^aeiou]qu|qu|[aeiou]/)
+      
+      if word_array[0] == ""
+        word_array.rotate!(2)
+      else
+        word_array.rotate!
+      end
+
+      word_rearranged = word_array.join()
+    end
+
+    word_rearranged << 'ay'
+  end
+
+  phrase_array_translated.join(' ')
+end

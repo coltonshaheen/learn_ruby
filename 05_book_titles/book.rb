a063fe7b7ab33790b163565490126e4a890f7f6c
Colton Shaheen <coltonjshaheen@gmail.com>
2018-11-03 11:24:34 -0400

    Finished all exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(new_title)
+    title_array = new_title.split(' ')
+
+    title_upcased = [title_array[0].capitalize!] + title_array[1..-1].map do |word|
+      capitalization_exceptions = ['a', 'an', 'and', 'or', 'but', 'in', 'the', 'of']
+      
+      if capitalization_exceptions.include? word
+        word
+      else
+        word = word.capitalize
+      end
+    end
+
+    @title = title_upcased.join(' ')
+  end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    remainder = @seconds % 3600
+    minutes = remainder / 60
+    seconds = remainder % 60
+
+    sprintf("%02i:%02i:%02i", hours, minutes, seconds)
+  end
+
 end
